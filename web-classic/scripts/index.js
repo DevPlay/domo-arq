8509a1128e0b149d76a357e5537ebc6db05da687
Nicolas <nicahdemarco@gmail.com>
2019-12-09 19:42:03 -0300

    zipeada carpeta web de webpack

--- web-classic/scripts/index.js
+++ web-classic/scripts/index.js
@@ -49,17 +49,17 @@
 //     .addClass('is-active is-completed');
 // });
 
-$(".mat-input").focus(function() {
+$(".mat-input").focus(function () {
   $(this)
     .parent()
     .addClass("is-active is-completed");
 });
 
-$(".mat-input").focusout(function() {
+$(".mat-input").focusout(function () {
   if ($(this).val() === "")
     $(this)
-      .parent()
-      .removeClass("is-completed");
+    .parent()
+    .removeClass("is-completed");
   $(this)
     .parent()
     .removeClass("is-active");

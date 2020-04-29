4ddb6372c386b7009740d8402b350225d4d060d6
Matias Cejas <matiascejas8@gmail.com>
2020-04-29 07:41:25 -0300

    Hooks added

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,16 @@
-import '../scss/app.scss';
+/* eslint-disable no-undef */
+/* eslint-disable no-param-reassign */
+ import '../scss/app.scss';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import '@fortawesome/fontawesome-free/js/all';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
 
-
 (function (global) {
-  // $ and jQuery are function here...
   global.$ = $;
+  // eslint-disable-next-line no-undef
   global.jQuery = jQuery;
-}(typeof window !== 'undefined' ? window : this));
+})(typeof window !== 'undefined' ? window : this);
 
 // Your JS Code goes here
 $('.mat-input').focus(function () {
